013b2a3d9bf6f11cde14026c929124bca68defb8
savimbi <kig@lI18>
2020-10-20 14:49:49 +0900

    Basic Authorization added

--- account/views.py
+++ account/views.py
@@ -1,18 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 
 
 def user_login(request):
     if request == 'POST':
-        form=LoginForm(request.POST)
+        form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
+            user = authenticate(
+                request, username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
-                    login(request,user)
+                    login(request, user)
                     return HttpResponse('Authenticated successfully')
                 else:
                     return HttpResponse('Desabled account')
@@ -20,9 +22,13 @@ def user_login(request):
                 return HttpResponse('Invalid login ')
     else:
         form = LoginForm()
-    return render(request, 'account/login.html', {'form':form})
+    return render(request, 'account/login.html', {'form': form})
 
-
+@login_required
+def dashboard(request):
+    return render(request, 
+                'account/dashboard.html',
+                {'section':'dashboard'})
 
 
 
